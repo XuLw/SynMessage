6d3b1a3cd7e23a164d9b2e548c758f1858656dbb
lwg <3590139958@qq.com>
2018-06-08 20:08:37 +0800

    添加BmobServer初始化的相关配置

--- BmobServer/bmobServerConfig.js
+++ BmobServer/bmobServerConfig.js
@@ -4,6 +4,10 @@ var bmobServerConfig={
       AsPersonal:"personal",
       AsReceiver:"receiver",
       AsPublisher:"publisher"
-  }
+  },
+  bmobKey:{
+    AppId:"ab7b1e4fb4e1b719ef52172960a8c6d5",
+    RestKey: "7b487f647c82ffaa4970f122d9e9d89f"
+  },
 }
 module.exports= bmobServerConfig;
